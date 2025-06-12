4b7383ab35bc800cce12c744ed529dd99c3f236e
Sushant <panditsushant11@gmail.com>
2025-06-12 14:58:12 +0530

    add hero section

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import Image from 'next/image';
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book, or rent a car - quickly and easily
+            </h1>
+
+        </div>
+      
     </div>
   )
 }
